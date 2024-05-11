505c8952e84dcecbb523ca9b55b5ea4ffce2d6f8
Mynor Alonzo <1yno3alonzo@gmail.com>
2024-05-10 20:56:31 -0600

    animaciones y colores

--- src/app/pages/authentication/authentication.module.ts
+++ src/app/pages/authentication/authentication.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -13,7 +14,9 @@ import { AuthenticationRoutingModule } from './authentication-routing.module';
   ],
   imports: [
     CommonModule,
-    AuthenticationRoutingModule
+    AuthenticationRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthenticationModule { }
--- src/app/pages/authentication/register/register.component.ts
+++ src/app/pages/authentication/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
  loading: Boolean = false;
+frmRegister = this.formBuilder.group({
+fistName: ['Mynor', Validators.required],
+lastName: ['Alonzo', Validators.required],
+username: ['1yno3alonzo@gmail.com', Validators.required],
+password: ['Mynor', Validators.required],
+reTypePassword: ['Mynor0', Validators.required],
+avatar: [null],
+terms: [false, Validators.required]
+})
+ constructor(private formBuilder: FormBuilder){
+
+ }
+ onSubmit(){
+  console.log(this.frmRegister);
+ }
 }
